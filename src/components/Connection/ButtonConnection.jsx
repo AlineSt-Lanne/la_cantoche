d18fb69c3e9ffb694b3768a445eb1f29963be4ef
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2023-05-23 17:17:13 +0200

    create page loginconnect, connection, createaccount & css

---
+++ src/components/Connection/ButtonConnection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/ButtonConnection.css";
+
+function ButtonConnection() {
+  return (
+    <div className="buttonconnection">
+      <h3>Se connecter</h3>
+    </div>
+  );
+}
+
+export default ButtonConnection;
---
+++ src/components/Connection/ButtonCreateAccount.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/ButtonCreateAccount.css";
+
+function ButtonCreateAccount() {
+  return (
+    <div className="buttoncreateaccount">
+      <h3>Créer son compte</h3>
+    </div>
+  );
+}
+
+export default ButtonCreateAccount;
