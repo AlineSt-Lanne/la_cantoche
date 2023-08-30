317248c33b32c3ba8c79dade5b1664c59fbd36b4
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2023-08-30 18:15:17 +0200

    create page profile

--- src/components/Profile/HeaderProfile.jsx
+++ src/components/Profile/HeaderProfile.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import "./styles/HeaderProfile.css";
 import KidPortrait from "../../assets/kid-portrait.png";
+import MaitressePortrait from "../../assets/maitresse-portrait.png";
 
 function HeaderProfile() {
   return (
     <div className="container-headerprofile">
+        <div className="container-headerprofile-infos">
+          <div className="headerprofile-infos-categories">
+            <h3 className="infos-categories">Age</h3>
+            <h3 className="infos-categories">Classe</h3>
+            <h3 className="infos-categories">Maîtresse</h3>
+          </div>
+          <div className="container-infos-profile">
+            <div className="headerprofile-infos-profile">
+              <h3 className="infos-profile">6 ans</h3>
+              <h3 className="infos-profile">CP B</h3>
+              <img className="image-maitresse" src={MaitressePortrait} alt="" />
+              <h3 className="infos-maitresse">Monique</h3>
+            </div>
+          </div>
+        </div>
         <div className="headerprofile-picture">
             <img className="image-titouan" src={KidPortrait} alt="" />
         </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import "./styles/Profile.css";
 import HeaderProfile from "../components/Profile/HeaderProfile";
+import AllergiesProfile from "../components/Profile/AllergiesProfile";
+import ContactPerson from "../components/Profile/ContactPerson";
+import Navbar from "../components/Home/Navbar";
 
 function Profile() {
     return (
       <div className="profile-container">
-        <div className="home-header">
-            <HeaderProfile />
+        <div className="profile-header">
+          <HeaderProfile />
+        </div>
+        <div className="profile-title-cantine">JOURS DE CANTINE</div>
+        <div className="profile-days-cantine">
+          <div className="days-cantine">Lundi - Mardi - Jeudi - Vendredi</div>
+        </div>
+        <div className="profile-title-cantine">RESTRICTIONS ALIMENTAIRES</div>
+        <div className="container-profile-allergies">
+          <AllergiesProfile />
+        </div>
+        <div className="profile-title-cantine">PERSONNE(S) À CONTACTER</div>
+          <div className="container-profile-contact">
+            <ContactPerson />
+          </div>
+        <div className="profile-container-navbar">
+          <Navbar />
         </div>
       </div>
     );
