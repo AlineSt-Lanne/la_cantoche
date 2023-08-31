d7d6dec82d46f2d375f519c405feb9c6cfaeb12c
Aline Saint-Lanne <aline.saint-lanne@orange.fr>
2023-08-31 15:17:50 +0200

    create page contact

--- src/components/Home/HeaderHome.jsx
+++ src/components/Home/HeaderHome.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import "./styles/HeaderHome.css";
 import imageplates from "../../assets/3d-plates.png";
-import ButtonReturn from "../Connection/ButtonReturn";
 
 function HeaderHome() {
   return (
     <div className="container-headerhome">
-        <div className="container-headerhome-orange">
-            <div className="header-arrow-return">
-                <ButtonReturn />
-            </div>
-            <div className="container-headerhome-title">
-                <img className="image-headerhome" src={imageplates} alt="" />
-                <div className="headerhome-title">CE JOUR</div>
-            </div>
+      <div className="container-headerhome-orange">
+        <div className="container-headerhome-title">
+          <img className="image-headerhome" src={imageplates} alt="" />
+          <div className="headerhome-title">CE JOUR</div>
         </div>
+      </div>
     </div>
   );
 }
